eb2a9d963c955912d908b4d1ae98660fa32f322d
luis barrios <luisbarrios0992@gmail.com>
2022-12-27 19:22:55 -0400

    fixed en share con navigator

--- resources/js/mixins/utilMixin.js
+++ resources/js/mixins/utilMixin.js
@@ -143,12 +143,12 @@ export default {
             navigator.clipboard.writeText(text).then(
                 function () {
                     console.log("Async: Copying to clipboard was successful!");
-                    this.noty("Copiado al portapapeles");
                 },
                 function (err) {
                     console.error("Async: Could not copy text: ", err);
                 }
             );
+            this.noty("Copiado al portapapeles");
         },
 
         /**
