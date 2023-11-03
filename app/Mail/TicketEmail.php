a71e703dd2d43fe289f49c46c71f77bc732dfb54
Augusto Alvarez <augustoalvarez05@gmail.com>
2023-11-03 12:05:45 -0400

    test qr

--- app/Mail/TicketEmail.php
+++ app/Mail/TicketEmail.php
@@ -46,9 +46,20 @@ class TicketEmail extends Mailable
         //$output = $writer->writeString('https://www.example.com');
         
         //$qrCodeBase64 = base64_encode($output);
+        $renderer = new RendererStyle(400);
+        $writer = new Writer(new Png(), $renderer);
+
+        // Define el contenido del QR
+        $content = 'https://www.example.com';
+
+        // Define el path donde se guardará la imagen, en el disco público.
         $path = 'qrcodes/event-ticket.png';
-        QrCode::format('png')->size(400)->generate('https://www.example.com', public_path($path));
-        $qrCodeUrl = asset($path);
+
+        // Guarda la imagen en el sistema de archivos público
+        Storage::disk('public')->put($path, $writer->writeString($content));
+
+        // Obtiene la URL pública para acceder a la imagen.
+        $qrCodeUrl = Storage::disk('public')->url($path);
         return $this->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'))
             ->subject('Ticket de evento - Myeart ' . env('APP_NAME'))
             ->view('emails.ticket')->with(['qrCode'=> $qrCodeUrl]);
